47a527da9f772eb033369855188a847970f5c36b
tusharfiet24 <ertusharfiet24@gmail.com>
2024-08-14 16:17:04 +0530

    Generate cucumber report for test execution results

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -6,8 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\java\\features",glue= {"stepDefinations"}, tags= "@DeletePlace")
+@CucumberOptions(features="src\\test\\java\\features", plugin = "json:target/jsonReports/cumcumber-report.json", glue= {"stepDefinations"}/*, tags= "@DeletePlace"*/)
 public class TestRunner {
 
 }
  
+// mvn test -Dcucumber.filter.tags="@AddPlace"
